e1f4347926acf3b27e4ff03b78f8efa80c98557a
heabeounMKTO <heabeoun69zin@gmail.com>
2024-10-21 11:52:01 +0700

    subfolders

--- hbpyutils.py
+++ hbpyutils.py
@@ -4,15 +4,16 @@ import random
 
 def get_file_by_extension(target_folder: str, extensions: tuple):
     '''
-    takes in a target folder and a tuple of extensions , 
-    returns a list of path strings
+    Takes in a target folder and a tuple of extensions,
+    returns a list of path strings of files with the specified extensions,
+    including those in subfolders.
     '''
     _a = []
-    for file in os.listdir(target_folder):
-        if file.endswith(extensions):
-            _a.append(os.path.join(target_folder, file))
+    for root, _, files in os.walk(target_folder):
+        for file in files:
+            if file.endswith(extensions):
+                _a.append(os.path.join(root, file))
     return _a
-
 
 def ratio_list(input_list: list, ratio: float = 0.7):
     '''
