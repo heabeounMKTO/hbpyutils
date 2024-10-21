12341e269262bb1d3ce6d167fda2b1918b597bef
heabeounMKTO <heabeoun69zin@gmail.com>
2024-10-21 11:49:02 +0700

    ratio

--- hbpyutils.py
+++ hbpyutils.py
@@ -1,6 +1,6 @@
 import os
-
-
+import math
+import random
 
 def get_file_by_extension(target_folder: str, extensions: tuple):
     '''
@@ -12,3 +12,22 @@ def get_file_by_extension(target_folder: str, extensions: tuple):
         if file.endswith(extensions):
             _a.append(os.path.join(target_folder, file))
     return _a
+
+
+def ratio_list(input_list: list, ratio: float = 0.7):
+    '''
+    separate list into two by ratio by selecting randomly
+    '''
+    _cp = input_list.copy()
+    _ratio_num = math.floor(len(_cp) * ratio)
+    _ratio_list = []
+    _sel_count = _ratio_num
+    for _ in range(_ratio_num):
+        random_index = random.randint(0, (_sel_count - 1))
+        _sel_count -= 1
+        _sel = input_list[random_index]
+        _ratio_list.append(_sel)
+        input_list.pop(input_list.index(_sel))
+    return _ratio_list, input_list
+        
+
